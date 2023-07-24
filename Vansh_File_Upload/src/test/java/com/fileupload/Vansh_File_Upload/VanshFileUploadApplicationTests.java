8f1ffd3ee443657e310e04b11c168a5aeac55f3c
Vansh Vatsal <114556099+vanshvatsal@users.noreply.github.com>
2023-07-24 12:39:16 +0530

    Add files via upload

---
+++ Vansh_File_Upload/src/test/java/com/fileupload/Vansh_File_Upload/VanshFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileupload.Vansh_File_Upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanshFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
